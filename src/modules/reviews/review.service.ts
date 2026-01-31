5bcf9fb24e6101213273981ab4750435ae16d816
mdarafatyeasin <yeasinarafat54239@gmail.com>
2026-01-31 10:58:58 +0600

    solved review error

--- src/modules/reviews/review.service.ts
+++ src/modules/reviews/review.service.ts
@@ -2,6 +2,27 @@ import { Review } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
 const crateReview = async (data: Omit<Review, "id" | "student_id" | "create_at" | "update_at">, user_id: string) => {
+    console.log(data.booking_id);
+
+    if(!user_id){
+        throw new Error("user is messing")
+    }
+
+    if(!data.booking_id){
+        throw new Error("booking id not found");
+    }
+
+    const isBookingExist = await prisma.booking.findUnique({
+        where:{
+            id:data.booking_id,
+            student_id:user_id
+        }
+    })
+
+    if(!isBookingExist){
+        throw new Error("Booking dose not exist");
+    }
+
     const result = await prisma.review.create({
         data: {
             ...data,
